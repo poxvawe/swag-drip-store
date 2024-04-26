798bb7471300d4ec4454785ca972bcb331310f68
baklazhan3000 <theky6ux@gmail.com>
2024-04-26 16:41:28 +0300

    канец

--- routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from main import app
-
-
-@app.route('/')
-def index():
-    products = Product.query.all()
-    products = []
-    return render_template('index.html', products=products)
